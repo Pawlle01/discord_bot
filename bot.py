437231fa4e60dc88847edf99d6c843143362cf53
Pawel Galler <Pawel.Galler@fdmgroup.com>
2025-08-17 17:16:23 +0100

    soundboard added, not working yet

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-import aiohttp
+import aiohttp, asyncio
 import datetime as dt
 from discord.ext import commands
 
@@ -36,13 +36,13 @@ async def hello(ctx):
 async def download_image(ctx, *, url):
     caller_name = str(ctx.author)
     if caller_name == "pawle":
-        async with aiohttp.ClientSession() as session:
 
+        async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
 
                 if response.status == 200:
                     data = await response.read()
-                    filename =  caller_name + dt.datetime.now().strftime(r"%Y-%m-%d_%H%M%S") + url.split("/")[-1]
+                    filename =  caller_name + dt.datetime.now().strftime(r"%Y-%m-%d_%H%M%S") + "_" + url.split("/")[-1]
 
                     with open(download_path+filename, "wb") as f:
                         f.write(data)
@@ -54,8 +54,35 @@ async def download_image(ctx, *, url):
     else:
         await ctx.send(f"Sorry, you are not permitted to use this command right now. Good try.")
 
+
 @bot.command()
-async def id_test(ctx):
-    await ctx.send(f"{ctx.author}")
+async def soundboard(ctx, sound_id):
+    if not ctx.author.voice:
+        await ctx.send("You are not currently in a voice channel.")
+        return
+    
+    channel = ctx.author.voice.channel
+    voice_client = await channel.connect()
+
+    filename = "soundboard/" + str(sound_id) + ".mp3"
+    audio_source = discord.FFmpegPCMAudio(executable=r"C:\Users\pgall\Downloads\ffmpeg-2025-08-14-git-cdbb5f1b93-essentials_build\bin\ffmpeg.exe", source=filename)
+    
+    def after_playing(error):
+        if error:
+            print(f"Error during playback: {error}")
+        coro = voice_client.disconnect()
+        fut = asyncio.run_coroutine_threadsafe(coro, bot.loop)
+        try:
+            fut.result()
+        except:
+            pass
+
+    voice_client.play(audio_source, after=after_playing)
+    await ctx.send(f"Playing {filename}")
+    
+    
+
+
+        
 
 bot.run(token)
