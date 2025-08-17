c44595999e652244c802d3cbc4168cd39520f35d
Pawel Galler <Pawel.Galler@fdmgroup.com>
2025-08-17 14:40:17 +0100

    Initial commit

---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+
+# Intents 
+intents = discord.Intents.default()
+intents.message_content = True
+
+print("Message Content Intent:", intents.message_content)
+print("Presences Intent:", intents.presences)
+print("Members Intent:", intents.members)
+
+
+# Prefix 
+bot = commands.Bot(command_prefix="Clanker!", intents=intents, case_insensitive=True)
+
+
+# Token
+token = ""
+with open("tokens", 'r') as file:
+    token = file.read().split("bot_token=", 1)[1]
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello World! I am Pawle's Goy-Clanker.")
+
+bot.run(token)
